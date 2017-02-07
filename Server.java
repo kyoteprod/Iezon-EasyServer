fc46bd253103fbcef1220259997d7a6fe07bdcb3
Kyle Thomas <kaydotm8@gmail.com>
2017-02-07 20:30:44 +0000

    Update 1.1 - Added one-factor authentication

--- Server.java
+++ Server.java
@@ -3,4 +3,6 @@ import java.io.IOException;
 public class Server {
 	// handler default init
 	public static RequestHandler rh = new RequestHandler();
+	// one factor authentication string for Update 1.1
+	public static String authenticationString = "change me";
 }
